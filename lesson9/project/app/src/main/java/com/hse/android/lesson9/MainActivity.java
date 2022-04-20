aebf29e1ad77f52bd91d9520789eac6e7cf91889
Ilya Makarov <i.o.makarov@2gis.ru>
2022-04-20 15:17:45 +0300

    Added lesson 9 demo

---
+++ lesson9/project/app/src/main/java/com/hse/android/lesson9/MainActivity.java
@@ -0,0 +1,105 @@
+package com.hse.android.lesson9;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+    private final String[] REQUIRED_PERMISSIONS = new String[] {
+            Manifest.permission.CAMERA,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+
+    private ImageCapture imageCapture = null;
+    private ImageAnalysis imageAnalysis = null;
+
+    private final ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    startCamera();
+                } else {
+                    Toast.makeText(this, "Camera permission required", Toast.LENGTH_LONG).show();
+                }
+            });
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (allPermissionsGranted()) {
+            // You can use the API that requires the permission.
+            startCamera();
+        } else {
+            // You can directly ask for the permission.
+            // The registered ActivityResultCallback gets the result of this request.
+            requestPermissionLauncher.launch(
+                    Manifest.permission.CAMERA);
+        }
+    }
+
+    private boolean allPermissionsGranted(){
+        for(String permission : REQUIRED_PERMISSIONS) {
+            if(ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void startCamera() {
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                bindPreview(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                // No errors need to be handled for this Future.
+                // This should never be reached.
+                assert false;
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    private void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+        Preview preview = new Preview.Builder()
+                .build();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        PreviewView view = findViewById(R.id.viewCameraPreview);
+        preview.setSurfaceProvider(view.getSurfaceProvider());
+
+        imageCapture = new ImageCapture.Builder()
+                .setTargetRotation(view.getDisplay().getRotation())
+                .build();
+
+        imageAnalysis = new ImageAnalysis.Builder()
+                .build();
+
+        Camera camera = cameraProvider
+                .bindToLifecycle(this, cameraSelector, imageCapture, imageAnalysis, preview);
+    }
+}
